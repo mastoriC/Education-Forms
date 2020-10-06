4eb106bc7bb2189537dab6a7c89adfae93bc87ff
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2020-10-06 18:42:52 +0700

    Add logo, title, and major options

--- src/form/Form.vue
+++ src/form/Form.vue
@@ -2,16 +2,34 @@
     <div class="container-fluid bg-kmitl-linear py-5 vh-100">
         <div class="container rounded-lg shadow h-100 p-5 bg-white">
             <div class="text-center">
-                <h3 class="mb-4">กรุณาเลือกคณะและสาขา</h3>
+                <div class="col-8 col-md-6 col-lg-4 offset-2 offset-md-3 offset-lg-4 px-5 mb-5">
+                    <img class="w-100" src="../../imgs/KMITL_logo.png" alt="">
+                </div>
+                <h1 class="font-weight-lighter">ระบบรายงานรายละเอียดรายวิชา</h1>
+                <h5>สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</h5>
+                <h4 class="mb-4 mt-5">กรุณาเลือกคณะและสาขา</h4>
                 <div class="col-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
                     <multiselect
-                        v-model="selected"
-                        :options="faculties"
-                        :custom-label="getNameTH"
+                        v-model="selectedFaculty"
+                        :options="getFaculties()"
+                        :custom-label="getFacTH"
                         select-label="เลือก"
                         deselect-label="ยกเลิก"
                         selected-label=""
-                        placeholder="เลือกคณะ">
+                        maxHeight="200"
+                        placeholder="คณะที่เลือก"
+                        class="my-3">
+                    </multiselect>
+                    <multiselect
+                        v-model="selectedMajor"
+                        :options="getMajors()"
+                        select-label="เลือก"
+                        deselect-label="ยกเลิก"
+                        selected-label=""
+                        maxHeight="200"
+                        placeholder="สาขาที่เลือก"
+                        class="my-3">
+                        <span slot="noOptions" class="text-secondary">ไม่สามารถเลือกสาขาได้</span>
                     </multiselect>
                 </div>
             </div>
@@ -30,13 +48,38 @@ export default {
     },
     data() {
         return {
-            selected: '',
-            faculties: JSON_faculties
+            selectedFaculty: '',
+            selectedMajor: '',
         }
     },
     methods: {
-        getNameTH({id, th}) {
+        getFacTH({id, th}) {
             return `[${id}] คณะ${th}`;
+        },
+
+        getFaculties() {
+            var facs = [];
+            for (const [id, details] of Object.entries(JSON_faculties)) {
+                facs.push({"id": id, "th": details.th});
+            }
+            facs.sort((a, b) => {
+                return parseInt(a.id) - parseInt(b.id);
+            });
+            return facs;
+        },
+
+        getMajors() {
+            var facId = this.selectedFaculty.id;
+            var majNames = [];
+            if (facId !== undefined) {
+                var majors = JSON_faculties[facId].majors;
+                if (majors !== undefined) {
+                    majors.forEach(maj => {
+                        majNames.push(maj.th);
+                    })
+                }
+            }
+            return majNames;
         }
     }
 }
